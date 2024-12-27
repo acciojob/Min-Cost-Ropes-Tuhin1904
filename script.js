1e0ffe58cfc06e8ba6c18f331184bece70c0ca02
Tuhin Ghosh <66065760+Tuhin1904@users.noreply.github.com>
2024-12-28 01:09:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+if(arr.length <= 1){
+	return 0;
+}
+	let totalcost = 0;
+
+	while (arr.length > 1) {
+		// console.log("Array in every stage: ",arr)
+		arr.sort((a,b) => a-b);
+
+		const rope1 = arr.shift();
+		const rope2 = arr.shift();
+
+		const cost = rope1 + rope2;
+		totalcost += cost;
+
+		arr.push(cost)
+		
+	}
+  return totalcost;
 }
 
 module.exports=mincost;
